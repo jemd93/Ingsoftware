ba7263f401526ed3b5c73053574b4b501637a121
jemd93 <jemd311@gmail.com>
2015-01-14 09:36:47 -0430

    Funcion lista

--- Main.py
+++ Main.py
@@ -23,18 +23,14 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
         print("Error : Fecha de salida menor que Fecha de entrada ")
          
     #if delta.days > 3 :
-       # print("Error : La reservacion no puede ser mayor a 72 horas.")
+       #print("Error : La reservacion no puede ser mayor a 72 horas.")
     #elif (delta.seconds/60) < 15 :
-     #   print("Error : La reservacion no puede ser menor a 15 minutos.")
+        #print("Error : La reservacion no puede ser menor a 15 minutos.")
         
     total = Decimal(0)
     totalHoras = delta.days*24 + ceil(delta.seconds/3600)
+    print("Total horas : ",totalHoras)
     horaActual = FechaIni.hour
-    
-    if (FechaIni.hour > 6) and (FechaIni.hour < 18) :
-        tarActual = tarif.tDiurna
-    else : 
-        tarActual = tarif.tNocturna
     
     while (totalHoras > 0) :
         horaActual = horaActual + 1
@@ -42,12 +38,16 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
         if (horaActual == 24) :
             total = total+tarif.tNocturna
             horaActual = 0    
+            print("1")
         elif (horaActual == 6) or (horaActual == 18) :
             total = total+tarMax
-        elif (horaActual > 6) or (horaActual <= 18) :
+            print("2")
+        elif (horaActual > 6) and (horaActual <= 18) :
             total = total+tarif.tDiurna
+            print("3")
         else :
             total = total+tarif.tNocturna
+            print("4")
             
         totalHoras = totalHoras-1
         
@@ -56,16 +56,15 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
         
         
         
-        
     
 
-anioI = int(input("Introduzca el año de ingreso : "))
+anioI = int(input("Introduzca el anio de ingreso : "))
 mesI = int(input("Introduzca el mes de ingreso : "))
 diaI = int(input("Introduzca el dia de ingreso : "))
 horaI = int(input("Introduzca la hora de ingreso : "))
 minutosI = int(input("Introduzca el minuto de ingreso : "))
 
-anioS = int(input("Introduzca el año de salida : "))
+anioS = int(input("Introduzca el anio de salida : "))
 mesS = int(input("Introduzca el mes de salida : "))
 diaS = int(input("Introduzca el dia de salida : "))
 horaS = int(input("Introduzca la hora de salida : "))
