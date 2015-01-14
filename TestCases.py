0275cbbc57a862dccb8020b5c702456556ba57f3
jemd93 <jemd311@gmail.com>
2015-01-14 10:22:50 -0430

    Archivo Pruebas creado

---
+++ TestCases.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 14, 2015
+
+@author: jemd93
+'''
+
+import unittest
+from Main import CalcularTotal
+
+class TestCalcularTotal(unittest.TestCase):
+    
+    # Aqui van las pruebas
