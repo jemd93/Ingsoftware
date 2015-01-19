60f6f5ff1365706c628f0e7aeee4a8aaa020a061
jemd93 <jemd311@gmail.com>
2015-01-19 18:39:26 -0430

    Cambiado el main

--- Main.py
+++ Main.py
@@ -18,28 +18,37 @@ class Tarifa() :
 def CalcularTotal(FechaIni, FechaSal, tarif):
     delta = FechaSal - FechaIni
     tarMax = max(tarif.tDiurna,tarif.tNocturna)
+    totalMin = (delta.days*24 + delta.seconds/3600) * 60
     
     if (delta.seconds/60 < 0) or (delta.days < 0) :
         print("Error : Fecha de salida menor que Fecha de entrada ")
         return -1
-    if delta.days > 3 :
+    if (delta.days == 3 and delta.seconds > 0) or (delta.days > 3) :
         print("Error : La reservacion no puede ser mayor a 72 horas.")
         return -1
-    elif (delta.seconds/60) < 15 :
+    elif (totalMin) < 15 :
         print("Error : La reservacion no puede ser menor a 15 minutos.")
         return -1
-    if (tarif<0.01):
-        print("Error: La tarifa no puede ser menor a 0.01")
+    if (tarif.tDiurna<0) or (tarif.tNocturna<0):
+        print("Error: La tarifa no puede ser menor a 0")
         return -1
-    elif (tarif>(2**32)-1):
+    elif (tarif.tDiurna>(2**32)-1) or (tarif.tNocturna>(2**32)-1):
         print("Error: El valor de la tarifa no es representable")
         return -1
         
-    total = Decimal(0)
     totalHoras = delta.days*24 + ceil(delta.seconds/3600)
-    print("Total horas : ",totalHoras)
+    total = Decimal(0)
     horaActual = FechaIni.hour
     
+    if ( 6 <= horaActual < 18):
+        esDiurno = True
+    else :
+        esDiurno = False
+        
+    if (totalHoras == 1):
+        if ((FechaSal.hour == 6) or (FechaSal.hour == 18)) and (FechaSal.minute != 0) : 
+            return tarMax
+        
     while (totalHoras > 0) :
         horaActual = horaActual + 1
         
@@ -47,11 +56,23 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
             total = total+tarif.tNocturna
             horaActual = 0    
         elif (horaActual == 6) or (horaActual == 18) :
-            total = total+tarMax
-        elif (horaActual > 6) and (horaActual <= 18) :
+            if (totalHoras == 1):
+                if (esDiurno):
+                    total = total + tarif.tDiurna
+                else :
+                    total = total + tarif.tNocturna
+            else :
+                esDiurno = not(esDiurno)
+                total = total + tarMax
+                
+        elif (horaActual > 6) and (horaActual < 18) :
             total = total+tarif.tDiurna
-        else :
+        else : 
             total = total+tarif.tNocturna
+            
+        if (total > ((2**32)-1)) :
+            print("Error : El total es mayor al maximo numero representable.")
+            return -1
             
         totalHoras = totalHoras-1
         
@@ -59,7 +80,6 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
         
     
 def main():
-    #try:
         anioI = int(input("Introduzca el anio de ingreso : "))
         mesI = int(input("Introduzca el mes de ingreso (1-12) : "))
         diaI = int(input("Introduzca el dia de ingreso : "))
@@ -74,18 +94,25 @@ def main():
         
         tarD = Decimal(input("Introduzca la tarifa diurna : "))
         tarN = Decimal(input("Introduzca la tarifa nocturna : "))
+        
         tar = Tarifa(tarD,tarN)
         
+        try :
+            fechaIng = datetime(anioI,mesI,diaI,horaI,minutosI)
+        except ValueError :
+                print("Error : La fecha de ingreso no es valida.")
+                return -1
         
-        fechaIng = datetime(anioI,mesI,diaI,horaI,minutosI)
-        fechaSal = datetime(anioS,mesS,diaS,horaS,minutosS)
+        try :       
+            fechaSal = datetime(anioS,mesS,diaS,horaS,minutosS)
+        except ValueError : 
+                print("Error : La fecha de salida no es valida.")
+                return -1
         
         total = CalcularTotal(fechaIng,fechaSal,tar)
+        
         if (total!=-1):
             print("Debe pagar: ",total)
-    #except:   
-        #print("Error con los argumentos de entrada")
-        
         
 if __name__ == "__main__":
     main()
