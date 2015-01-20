6c5f63b4e26443b766542a60b989f03f75fbf5b4
vickyjm <mvjorgemauriello@gmail.com>
2015-01-19 20:31:02 -0430

    Todo listo

--- Main.py
+++ Main.py
@@ -18,10 +18,6 @@ class Tarifa() :
 def CalcularTotal(FechaIni, FechaSal, tarif):
     delta = FechaSal - FechaIni
     tarMax = max(tarif.tDiurna,tarif.tNocturna)
-<<<<<<< HEAD
-=======
-    totalHoras = delta.days*24 + ceil(delta.seconds/3600)
->>>>>>> refs/heads/PruebasVicky
     totalMin = (delta.days*24 + delta.seconds/3600) * 60
     
     if (delta.seconds/60 < 0) or (delta.days < 0) :
@@ -30,39 +26,19 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
     if (delta.days == 3 and delta.seconds > 0) or (delta.days > 3) :
         print("Error : La reservacion no puede ser mayor a 72 horas.")
         return -1
-<<<<<<< HEAD
     elif (totalMin) < 15 :
-=======
-    elif (totalMin < 15) :
->>>>>>> refs/heads/PruebasVicky
         print("Error : La reservacion no puede ser menor a 15 minutos.")
         return -1
-<<<<<<< HEAD
     if (tarif.tDiurna<0) or (tarif.tNocturna<0):
-=======
-    if (tarif.tDiurna < 0) or (tarif.tNocturna < 0):
->>>>>>> refs/heads/PruebasVicky
         print("Error: La tarifa no puede ser menor a 0")
         return -1
     elif (tarif.tDiurna>(2**32)-1) or (tarif.tNocturna>(2**32)-1):
         print("Error: El valor de la tarifa no es representable")
         return -1
         
-<<<<<<< HEAD
     totalHoras = delta.days*24 + ceil(delta.seconds/3600)
-=======
->>>>>>> refs/heads/PruebasVicky
     total = Decimal(0)
     horaActual = FechaIni.hour
-    
-    if (6 <= horaActual < 18): # La hora inicial esta en el turno diurno
-        esDiurno = True         # Booleano para saber el turno actual
-    else:                       # La hora inicial esta en el turno nocturno
-        esDiurno = False 
-    
-    if (totalHoras == 1):
-        if ((FechaSal.hour == 6) or (FechaSal.hour == 18)) and (FechaSal.minute!=0): # Menos de una hora y mixta
-            return tarMax
     
     if ( 6 <= horaActual < 18):
         esDiurno = True
@@ -77,10 +53,9 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
         horaActual = horaActual + 1
         
         if (horaActual == 24) :
-            total = total + tarif.tNocturna
+            total = total+tarif.tNocturna
             horaActual = 0    
         elif (horaActual == 6) or (horaActual == 18) :
-<<<<<<< HEAD
             if (totalHoras == 1):
                 if (esDiurno):
                     total = total + tarif.tDiurna
@@ -93,30 +68,11 @@ def CalcularTotal(FechaIni, FechaSal, tarif):
         elif (horaActual > 6) and (horaActual < 18) :
             total = total+tarif.tDiurna
         else : 
-=======
-            if (totalHoras == 1): # Es la ultima hora que se cobrara
-                if (esDiurno):
-                    total = total + tarif.tDiurna
-                else:
-                    total = total + tarif.tNocturna
-            else: # Queda mas tiempo en la reservacion, pero cambio de turno
-                esDiurno = not(esDiurno)               
-                total = total + tarMax
-        elif (horaActual > 6) and (horaActual < 18) :
-            total = total + tarif.tDiurna
-        else :
->>>>>>> refs/heads/PruebasVicky
             total = total+tarif.tNocturna
             
-<<<<<<< HEAD
         if (total > ((2**32)-1)) :
             print("Error : El total es mayor al maximo numero representable.")
             return -1
-=======
-        if (total > ((2**32)-1)):
-            print("Error : El total es mayor al maximo numero representable.")
-            return -1    
->>>>>>> refs/heads/PruebasVicky
             
         totalHoras = totalHoras-1
         
@@ -157,11 +113,6 @@ def main():
         
         if (total!=-1):
             print("Debe pagar: ",total)
-<<<<<<< HEAD
-=======
-    #except:   
-        #print("Error con los argumentos de entrada")
->>>>>>> refs/heads/PruebasVicky
         
 if __name__ == "__main__":
     main()
