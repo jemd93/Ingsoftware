29e62191c24c757ff712d0ac4269150222305e6f
jemd93 <jemd311@gmail.com>
2015-01-27 15:51:51 -0430

    Primer commit TDD Jorge

---
+++ Tarea3/Estacionamiento.py
@@ -0,0 +1,20 @@
+'''
+Created on Jan 27, 2015
+
+Jorge Marcano : 11-10566
+Maria Victoria Jorge : 11-10566
+
+'''
+
+class Estacionamiento :
+    
+    def __init__(self,puestos): 
+        self.reservaciones = []
+        self.puestos = puestos
+        
+    def reservar(self,horaIni,horaFin) :
+        return True
+        
+        
+    
+    
---
+++ Tarea3/SuiteTDD.py
@@ -0,0 +1,21 @@
+'''
+Created on Jan 27, 2015
+
+Jorge Marcano : 11-10566
+Maria Victoria Jorge : 11-10566
+
+'''
+import unittest
+from Estacionamiento import Estacionamiento
+
+class TestReservar(unittest.TestCase):
+    
+    def testExistenciaFuncion(self):
+        e = Estacionamiento(10)
+        e.reservar(10,15)
+        
+    def testReservacionVacio(self):
+        e = Estacionamiento(10)
+        e.reservaciones = []
+        self.assertEquals(e.reservar(10,12),True)
+
